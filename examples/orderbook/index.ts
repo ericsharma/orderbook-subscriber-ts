7670491055b307c19f4856b3647471119ab946fc
ericsharma <sharma.e@husky.neu.edu>
2024-02-16 13:19:04 -0500

    ✅ Simple working example of saving to db

--- examples/orderbook/index.ts
+++ examples/orderbook/index.ts
@@ -3,8 +3,15 @@ import { TransactionResult } from '@algorandfoundation/algokit-utils/types/index
 import algosdk, { TransactionType } from 'algosdk'
 import fs from 'fs'
 import path from 'path'
+import prisma from '../../prisma/prisma'
 import { AlgorandSubscriber } from '../../src/subscriber'
 
+function smaller(a: number, b: number) {
+  return a < b ? a : b
+}
+function bigger(a: number, b: number) {
+  return a > b ? a : b
+}
 if (!fs.existsSync(path.join(__dirname, '..', '..', '.env')) && !process.env.ALGOD_SERVER) {
   // eslint-disable-next-line no-console
   console.error('Copy /.env.sample to /.env before starting the application.')
@@ -49,46 +56,94 @@ async function getOrderBookSubscriber() {
 async function saveOrderbookTransactions(transactions: TransactionResult[]) {
   // eslint-disable-next-line no-debugger
   debugger
+  // eslint-disable-next-line
+  if (!transactions[2]['inner-txns']) return
+  console.info('H*****')
+  const testAssets = transactions[2]['inner-txns'][0]['foreign-assets']
+  console.info(testAssets)
+
+  // await prisma.tradingPair.create({ data: { olderAssetId: '1194', newerAssetId: '1195' } })
+
+  const transactionsWithAppArgs = transactions.map(async (transaction) => {
+    // xGwzBQ==
+
+    if (transaction['application-transaction'] === undefined) return transaction
+    if (transaction['application-transaction']['application-args'] === undefined) return transaction
+
+    const methodIdentifier = transaction?.['application-transaction']?.['application-args'][0]
+
+    if (methodIdentifier === 'xGwzBQ==' && transaction['application-transaction']['foreign-assets']) {
+      transaction['application-transaction']['application-args'] = [
+        'orderbook sell Asset OptIn',
+        `${transaction['application-transaction']['foreign-assets'][0]}`,
+      ]
+    }
+
+    if (methodIdentifier === 'OBFgNw==') {
+      const appArgs = transaction?.['application-transaction']['application-args']
+      const decoded = [
+        'User application call to Orderbook',
+        Buffer.from(appArgs[1], 'base64').toString(),
+        Buffer.from(appArgs[2], 'base64').toString(),
+        ` ${algosdk.decodeUint64(Buffer.from(appArgs[3], 'base64'), 'safe')}`,
+        ` ${algosdk.decodeUint64(Buffer.from(appArgs[4], 'base64'), 'safe')}`,
+      ]
+
+      transaction['application-transaction']['application-args'] = decoded
+      if (!transaction['inner-txns']) return transaction
+      const orderCreateInnerTxn = transaction['inner-txns'][0]['application-transaction']
+      if (!orderCreateInnerTxn) return transaction
+      if (!orderCreateInnerTxn['application-args']) return transaction
+      if (!orderCreateInnerTxn['foreign-assets']) return transaction
+      if (!orderCreateInnerTxn['accounts']) return transaction
+
+      const innerAppArgs = orderCreateInnerTxn['application-args']
+      const decodedInner = [
+        'Orderbook create Order app call',
+        ` ${orderCreateInnerTxn['foreign-assets'][0]}`,
+        `${orderCreateInnerTxn['foreign-assets'][1]}`,
+        ` ${algosdk.decodeUint64(Buffer.from(innerAppArgs[3], 'base64'), 'safe')}`,
+        ` ${algosdk.decodeUint64(Buffer.from(innerAppArgs[4], 'base64'), 'safe')}`,
+        orderCreateInnerTxn['accounts'][0],
+      ]
+      if (!transaction['inner-txns'][0]['application-transaction']) return transaction
+      transaction['inner-txns'][0]['application-transaction']['application-args'] = decodedInner
+
+      if (!transaction['inner-txns'][1]['payment-transaction']) return transaction
+      const appPayTxn = transaction['inner-txns'][1]['payment-transaction']
+      const olderAssetId = smaller(Number(decodedInner[1]), Number(decodedInner[2])).toString()
+      const newerAssetId = bigger(Number(decodedInner[1]), Number(decodedInner[2])).toString()
+
+      console.info('H*****')
+      console.info(olderAssetId, newerAssetId)
+      await prisma.order.create({
+        data: {
+          // tradingPair: {
+          //   connect: { tradingPairId: { olderAssetId: olderAssetId, newerAssetId: newerAssetId } },
+          // },
+          tradingPair: {
+            connectOrCreate: {
+              where: { tradingPairId: { olderAssetId, newerAssetId } },
+              create: { olderAssetId, newerAssetId },
+            },
+          },
+          sellAssetId: decodedInner[1],
+          buyAssetId: decodedInner[2],
+          sellQuant: decodedInner[3],
+          buyQuant: decodedInner[4],
+          owner: decodedInner[5],
+          appAddress: appPayTxn['receiver'],
+          type: decoded[1] === '1194' ? 'sell' : 'buy',
+        },
+      })
+    }
+
+    return transaction
+  })
+
   await saveTransactions(
-    transactions.map((transaction) => {
-      // xGwzBQ==
+    transactionsWithAppArgs,
 
-      if (transaction?.['application-transaction']['application-args'][0] === 'xGwzBQ==') {
-        transaction['application-transaction']['application-args'] = [
-          'orderbook sell Asset OptIn',
-          `${transaction['application-transaction']['foreign-assets'][0]}`,
-        ]
-      }
-
-      if (transaction?.['application-transaction']['application-args'][0] === 'OBFgNw==') {
-        const appArgs = transaction?.['application-transaction']['application-args']
-        const decoded = [
-          'User application call to Orderbook',
-          Buffer.from(appArgs[1], 'base64').toString(),
-          Buffer.from(appArgs[2], 'base64').toString(),
-          ` ${algosdk.decodeUint64(Buffer.from(appArgs[3], 'base64'), 'safe')}`,
-          ` ${algosdk.decodeUint64(Buffer.from(appArgs[4], 'base64'), 'safe')}`,
-        ]
-
-        transaction['application-transaction']['application-args'] = decoded
-        const orderCreateInnerTxn = transaction['inner-txns'][0]['application-transaction']
-
-        const innerAppArgs = orderCreateInnerTxn['application-args']
-
-        const decodedInner = [
-          'Orderbook create Order app call',
-          ` ${orderCreateInnerTxn['foreign-assets'][0]}`,
-          `${orderCreateInnerTxn['foreign-assets'][1]}`,
-          ` ${algosdk.decodeUint64(Buffer.from(innerAppArgs[3], 'base64'), 'safe')}`,
-          ` ${algosdk.decodeUint64(Buffer.from(innerAppArgs[4], 'base64'), 'safe')}`,
-          orderCreateInnerTxn['accounts'][0],
-        ]
-
-        transaction['inner-txns'][0]['application-transaction']['application-args'] = decodedInner
-      }
-
-      return transaction
-    }),
     'orderbook.json',
   )
 }
